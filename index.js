b428fd3b034ac68283b057809493eabbe7999c42
M.Usama <usamazarishah@gmail.com>
2025-01-03 22:18:11 +0500

    todo-app-backend-done

---
+++ index.js
@@ -0,0 +1,107 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = process.env.PORT || 5000
+const todos = []
+
+ //to convert body into JSON 
+app.use(express.json());
+
+app.use(cors({ origin: ["http://localhost:5173"]}));
+
+
+// get all todos api
+app.get('/all-todos', (req, res) => {
+
+  const message = !todos.length ? "todo empty" : "all todos fetched"
+
+  res.status(200).send({data : todos, message : message});
+}),
+
+
+// add todo api
+app.post('/add-todo', (req, res) => {
+    
+    const todo = req.body.todo
+    const obj = {
+      todoContent : todo,
+      id : String(new Date().getTime())
+    } 
+
+    todos.push(obj)
+    res.status(201).send({data:obj, message:"todo is added successfully!"})
+}),
+
+
+// edit or update todo api
+app.patch('/edit-todo/:id', (req, res) => {
+   const id = req.params.id
+   
+   let isFound = false;
+   for(let i = 0; i < todos.length; i++){
+
+    if (todos[i].id === id) {
+      //idher todo mil gaya ha (ab usko update karna ha)
+
+      todos[i].todoContent = req.body.todoContent
+      isFound = true
+      break;
+    }
+    
+   }
+
+   if(isFound){
+    res.status(201).send({
+      data:{todoContent: req.body.todoContent, id: id},
+      message:'todo is updated successfully!'
+    })
+
+   }else{
+    res.status(200).send({data:null, message:'todo not found!'})
+
+   }
+
+}),
+
+
+// delete todo api
+app.delete('/delete-todo/:id', (req, res) => {
+  const id = req.params.id
+   
+  let isFound = false;
+  for(let i = 0; i < todos.length; i++){
+
+   if (todos[i].id === id) {
+     //idher todo mil gaya ha (ab usko delete karna ha)
+
+     todos.splice(i, 1)
+
+     isFound = true
+     break;
+   }
+
+  }
+
+  if(isFound){
+   res.status(201).send({
+    //  data:{todoContent: req.body.todoContent, id: id},
+     message:'todo is deleted successfully!',
+   })
+
+  }else{
+   res.status(200).send({data:null, message:'todo not found!'})
+
+  }
+
+}),
+
+
+// no route found api
+app.use((req, res) => {
+    res.status(404).send('no route found!')
+}),
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
